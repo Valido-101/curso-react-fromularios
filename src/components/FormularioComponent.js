c3ca93b0effe42c0d9f5a18ef2fb80dc7bafd458
Valido-101 <jesuszafra22@gmail.com>
2023-01-21 18:11:37 +0100

    Formulario creado y maquetado

---
+++ src/components/FormularioComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const FormularioComponent = () => {
+  return (
+    <div>
+        <h1>
+            Formularios con React
+        </h1>
+
+        <form>
+            <input type="text" placeholder='Nombre'/>
+            <input type="text" placeholder='Apellidos'/>
+            <select>
+                <option value='hombre'>Hombre</option>
+                <option value='mujer'>Mujer</option>
+            </select>
+            <textarea placeholder='Biografía'></textarea>
+
+            <input type='submit' value='Enviar'/>
+        </form>
+    </div>
+  )
+}
